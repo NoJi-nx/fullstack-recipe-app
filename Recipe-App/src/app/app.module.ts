802381966aac21d813a2ba2f16a660ae3aa2ad0c
Nejirwan <37329734+NRiCe31@users.noreply.github.com>
2023-05-22 02:48:22 +0200

    lite ändringar

--- Recipe-App/src/app/app.module.ts
+++ Recipe-App/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +20,8 @@ import { RecipeAllComponent } from './recipe-all/recipe-all.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Recipe-App/src/app/recipe-list/recipe-list.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { RecipeListRoutingModule } from './recipe-list-routing.module';
+import { RecipeItemComponent } from '../recipe-item/recipe-item.component';
+import { RecipeAllComponent } from '../recipe-all/recipe-all.component';
+
+
+
+@NgModule({
+  declarations: [
+
+    RecipeItemComponent,
+    RecipeAllComponent
+  ],
+  imports: [
+    CommonModule,
+    RecipeListRoutingModule
+  ]
+})
+export class RecipeListModule { }
